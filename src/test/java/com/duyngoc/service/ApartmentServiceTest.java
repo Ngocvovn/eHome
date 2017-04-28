35b2065f5c900de808bcad8ba8aa469d5af35273
Ngocvovn <duyngocvo@gmail.com>
2017-04-28 20:20:03 +0300

    Add test cases  for Apartment Service

--- src/test/java/com/duyngoc/service/ApartmentServiceTest.java
+++ src/test/java/com/duyngoc/service/ApartmentServiceTest.java
@@ -1,9 +1,11 @@
 package com.duyngoc.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import org.junit.Before;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +23,60 @@ public class ApartmentServiceTest {
 	
 
 	@Test
-	public void saveSuccessfully() throws Exception{
+	public void saveSuccessfully(){
+		Apartment tes = new Apartment();
+		Apartment apartment= apartmentService.save(tes);
+		assertNotNull(apartment);
+	}
+	
+	@Test
+	public void setAndSaveSuccessfully(){
 		Apartment tes = new Apartment();
 		tes.setId((long) 20);
 		tes.setStreet("lintu");
 		Apartment apartment= apartmentService.save(tes);
 		assertEquals("lintu", apartment.getStreet());
 		assertNull(apartment.getImageUrls());
+	}
+	
+	@Test
+	public void getApartmentsByOwnerSuccessfully(){
+		Apartment tes = new Apartment();
+		tes.setId((long) 20);
+		tes.setStreet("lintu");
+		tes.setOwner("ngoc");
+		apartmentService.save(tes);
+		List<Apartment> apartments= apartmentService.getApartmentsByOwner("ngoc");
+		assertNotNull(apartments);
+		assertEquals(1, apartments.size());
+		assertEquals("ngoc", apartments.get(0).getOwner());
+		
+		
+	}
+	
+	public void testCustomSearchFullParameter(){
+		Apartment tes = new Apartment();
+		tes.setCity("Helsinki");
+		tes.setStreet("Lintu");
+		tes.setPrice(3000);
+		tes.setBedrooms(4);
+		apartmentService.save(tes);
+		List<Apartment> apartments = apartmentService.customSearch("Helsinki", "Lintu", 1000, 4000, 4);
+		assertNotNull(apartments);
+		assertEquals(1, apartments.size());
+		assertEquals("Helsinki", apartments.get(0).getCity());
+	}
+	
+	public void testCustomSearchDifferentBedrooms(){
+		Apartment tes = new Apartment();
+		tes.setCity("Helsinki");
+		tes.setStreet("Lintu");
+		tes.setPrice(3000);
+		tes.setBedrooms(4);
+		apartmentService.save(tes);
+		List<Apartment> apartments = apartmentService.customSearch("Helsinki", "Lintu", 1000, 4000, 5);
+		assertNotNull(apartments);
+		assertEquals(0, apartments.size());
 	}
 	
 	

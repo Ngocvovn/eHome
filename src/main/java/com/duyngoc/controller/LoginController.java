a7a0c7a26cb73cab1d0a5b7fafef65465bd6e191
Ngocvovn <duyngocvo@gmail.com>
2017-05-06 18:19:49 +0300

    current user end point

--- src/main/java/com/duyngoc/controller/LoginController.java
+++ src/main/java/com/duyngoc/controller/LoginController.java
@@ -5,6 +5,8 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,7 @@ import com.duyngoc.model.User;
 import com.duyngoc.repository.UserRepository;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 @CrossOrigin
 public class LoginController {
 	
@@ -22,7 +24,7 @@ public class LoginController {
 	private UserRepository userRepository;
 	
 
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(value="/login",method=RequestMethod.POST)
 	public ResponseEntity<?> login(Principal principal){
 		try {
 			System.out.println("login "+ principal.getName());
@@ -32,6 +34,19 @@ public class LoginController {
 			return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
+	}
+	
+	public ResponseEntity<?> getCurrentUser(){
+		try {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+			User user = userRepository.findByUsername(auth.getName());
+			return new ResponseEntity<User>(user, HttpStatus.OK);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<Exception>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 	
